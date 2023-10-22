bf2d75419fac619e1aa83ae2f6e30152f5b22234
kushagra-aa <kushagraagnihotri24@gmial.com>
2023-10-22 12:11:12 +0530

    fixed multi select wrapping

--- src/warpTag.ts
+++ src/warpTag.ts
@@ -5,18 +5,26 @@ const wrapTag = async (editor: TextEditor | undefined, tag: string) => {
   if (!selections) {
     return;
   }
-  selections.forEach(async (selection) => {
-    const selectionText = editor.document.getText(selection);
+  console.log("selections :>> ", selections);
+  editor.edit((e) => {
+    selections.forEach(async (selection, i) => {
+      console.log(
+        `selection-${i} :>> `,
+        `startline: ${selection.start.line} endline: ${selection.end.line}`
+      );
+      const selectionText = editor.document.getText(selection);
+      console.log("selectionText :>> ", selectionText);
+      const outputString = `<${tag}>${selectionText}</${tag}>`;
+      console.log("outputString :>> ", outputString);
 
-    editor.edit((e) =>
-      e.replace(selection, `<${tag}>${selectionText}</${tag}>`)
-    );
+      e.replace(selection, outputString);
 
-    try {
-      await commands.executeCommand("editor.action.formatDocument");
-    } catch (err) {
-      console.log("Formatting Error:", err);
-    }
+      try {
+        await commands.executeCommand("editor.action.formatDocument");
+      } catch (err) {
+        console.log("Formatting Error:", err);
+      }
+    });
   });
 };
 
